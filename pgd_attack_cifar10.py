14a904cfc839671eb33d370d4fefbbf416786f43
yuandaw <54134506+yuandaw@users.noreply.github.com>
2021-04-26 14:58:56 -0400

    Update pgd_attack_cifar10.py
    project

--- pgd_attack_cifar10.py
+++ pgd_attack_cifar10.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import os
 import argparse
+import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -49,16 +50,34 @@ kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
 transform_test = transforms.Compose([transforms.ToTensor(),])
 testset = torchvision.datasets.CIFAR10(root='../data', train=False, download=True, transform=transform_test)
 test_loader = torch.utils.data.DataLoader(testset, batch_size=args.test_batch_size, shuffle=False, **kwargs)
+def plot_dataset_digits(dataset, name, k):
+    fig = plt.figure(figsize=(13, 8))
+    columns = 3
+    rows = 2
+    # ax enables access to manipulate each of subplots
+    ax = []
 
+    for i in range(columns * rows):
+        img = dataset[i]
+        # create subplot and append to ax
+        ax.append(fig.add_subplot(rows, columns, i + 1))
+        ax[-1].set_title("CIFAR")  # set title
+        plt.imshow(img)
+
+    # plt.show()  # finally, render the plot
+    plt.savefig('./results/img'+name+'{}'.format(k)+'.png')
 
 def _pgd_whitebox(model,
                   X,
                   y,
+                  k,
                   epsilon=args.epsilon,
                   num_steps=args.num_steps,
                   step_size=args.step_size):
     out = model(X)
     err = (out.data.max(1)[1] != y.data).float().sum()
+    #loss_natural = nn.CrossEntropyLoss()(model(X), y)
+    # print('natural loss: ', loss_natural)
     X_pgd = Variable(X.data, requires_grad=True)
     if args.random:
         random_noise = torch.FloatTensor(*X_pgd.shape).uniform_(-epsilon, epsilon).to(device)
@@ -70,6 +89,7 @@ def _pgd_whitebox(model,
 
         with torch.enable_grad():
             loss = nn.CrossEntropyLoss()(model(X_pgd), y)
+            # print('attack loss: ', loss)
         loss.backward()
         eta = step_size * X_pgd.grad.data.sign()
         X_pgd = Variable(X_pgd.data + eta, requires_grad=True)
@@ -77,39 +97,41 @@ def _pgd_whitebox(model,
         X_pgd = Variable(X.data + eta, requires_grad=True)
         X_pgd = Variable(torch.clamp(X_pgd, 0, 1.0), requires_grad=True)
     err_pgd = (model(X_pgd).data.max(1)[1] != y.data).float().sum()
-    print('err pgd (white-box): ', err_pgd)
-    return err, err_pgd
-
-
-def _pgd_blackbox(model_target,
-                  model_source,
-                  X,
-                  y,
-                  epsilon=args.epsilon,
-                  num_steps=args.num_steps,
-                  step_size=args.step_size):
-    out = model_target(X)
-    err = (out.data.max(1)[1] != y.data).float().sum()
-    X_pgd = Variable(X.data, requires_grad=True)
+    perturbation = X_pgd - X
+    #########################################
+    X_p = Variable(X_pgd.data, requires_grad=True)
     if args.random:
-        random_noise = torch.FloatTensor(*X_pgd.shape).uniform_(-epsilon, epsilon).to(device)
-        X_pgd = Variable(X_pgd.data + random_noise, requires_grad=True)
+        #先一个noise 均匀分布
+        random_noise_2 = torch.FloatTensor(*X_p.shape).uniform_(-epsilon, epsilon).to(device)
+        #生成最初的样本
+        X_p = Variable(X_p.data + random_noise_2, requires_grad=True)
 
     for _ in range(num_steps):
-        opt = optim.SGD([X_pgd], lr=1e-3)
+        opt = optim.SGD([X_p], lr=1e-3)
         opt.zero_grad()
-        with torch.enable_grad():
-            loss = nn.CrossEntropyLoss()(model_source(X_pgd), y)
-        loss.backward()
-        eta = step_size * X_pgd.grad.data.sign()
-        X_pgd = Variable(X_pgd.data + eta, requires_grad=True)
-        eta = torch.clamp(X_pgd.data - X.data, -epsilon, epsilon)
-        X_pgd = Variable(X.data + eta, requires_grad=True)
-        X_pgd = Variable(torch.clamp(X_pgd, 0, 1.0), requires_grad=True)
 
-    err_pgd = (model_target(X_pgd).data.max(1)[1] != y.data).float().sum()
-    print('err pgd black-box: ', err_pgd)
-    return err, err_pgd
+        with torch.enable_grad():
+            loss = nn.CrossEntropyLoss()(model(X_p), y)
+            #print('recover loss: ', loss)
+        loss.backward()
+        eta = - step_size * X_p.grad.data.sign()
+        X_p = Variable(X_p.data + eta, requires_grad=True)#前面有random的noise
+        eta = torch.clamp(X_p.data - X_pgd.data, -epsilon, epsilon)
+        X_p = Variable(X_pgd.data + eta, requires_grad=True)
+        X_p = Variable(torch.clamp(X_p, 0, 1.0), requires_grad=True)
+        #向符号方向走
+    per_red= X_p-X_pgd
+    delta_p = per_red-perturbation
+    delta_per = torch.norm(delta_p, dim=2)
+    err_red = (model(X_p).data.max(1)[1] != y.data).float().sum()
+    plot_dataset_digits(X.cpu().detach().numpy(), '_natural_',k)
+    plot_dataset_digits(X_pgd.cpu().detach().numpy(), '_adv_',k)
+    plot_dataset_digits(X_p.cpu().detach().numpy(), '_red_',k)
+    #print('err pgd (white-box): ', err_pgd)
+
+    return delta_per, err, err_red, err_pgd
+
+
 
 
 def eval_adv_test_whitebox(model, device, test_loader):
@@ -117,6 +139,9 @@ def eval_adv_test_whitebox(model, device, test_loader):
     evaluate model by white-box attack
     """
     model.eval()
+    per_err_total = 0
+
+    natural_err_total = 0
     robust_err_total = 0
     natural_err_total = 0
 
@@ -124,31 +149,18 @@ def eval_adv_test_whitebox(model, device, test_loader):
         data, target = data.to(device), target.to(device)
         # pgd attack
         X, y = Variable(data, requires_grad=True), Variable(target)
-        err_natural, err_robust = _pgd_whitebox(model, X, y)
-        robust_err_total += err_robust
+        delta_per,err_natural, err_recovered, err_attack = _pgd_whitebox(model, X, y,k)
+        per_err_total += delta_per
+        recovered_err_total += err_recovered
+        attack_err_total += err_attack
         natural_err_total += err_natural
+        k+=1
+    print('natural_err_total: ', natural_err_total)
     print('natural_err_total: ', natural_err_total)
     print('robust_err_total: ', robust_err_total)
 
 
-def eval_adv_test_blackbox(model_target, model_source, device, test_loader):
-    """
-    evaluate model by black-box attack
-    """
-    model_target.eval()
-    model_source.eval()
-    robust_err_total = 0
-    natural_err_total = 0
 
-    for data, target in test_loader:
-        data, target = data.to(device), target.to(device)
-        # pgd attack
-        X, y = Variable(data, requires_grad=True), Variable(target)
-        err_natural, err_robust = _pgd_blackbox(model_target, model_source, X, y)
-        robust_err_total += err_robust
-        natural_err_total += err_natural
-    print('natural_err_total: ', natural_err_total)
-    print('robust_err_total: ', robust_err_total)
 
 
 def main():
